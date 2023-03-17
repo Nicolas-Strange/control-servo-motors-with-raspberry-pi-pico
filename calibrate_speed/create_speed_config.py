fix time calculation

calibrate_speed/create_speed_config.py
@@ -1,4 +1,5 @@
 import json
+from typing import Optional
 
 import matplotlib.pyplot as plt
 import numpy as np
@@ -29,13 +30,14 @@ def model(params: list, x: list) -> list:
 
 
 def mse(params: list, x: list, y: list) -> float:
-    """ mean absolute error calculation between the real values and the calculated values"""
+    """ mean squared error calculation between the real values and the calculated values"""
     y_p = model(params, x)
 
     return mean_squared_error(y, y_p)
 
 
 def plot(x: list, y: list, y_p: list) -> None:
+    """ plot a 2D graph """
     fig = plt.figure()
     ax = fig.add_subplot()
 
@@ -51,14 +53,14 @@ def plot(x: list, y: list, y_p: list) -> None:
 
 
 def plot_3d(x: list, y: list, y_p: list) -> None:
-    """ plot aa 3D graph """
+    """ plot a 3D graph """
     fig = plt.figure()
     ax = fig.add_subplot(projection='3d')
 
-    cond = np.where(x[0] < 100)
-    x_1 = x[1][cond]
-    x_2 = x[0][cond]
-    y_p_2 = y_p[cond]
+    # cond = np.where(x[0] < 100)
+    x_1 = x[1]
+    x_2 = x[0]
+    y_p_2 = y_p
 
     ax.scatter(x[0], x[1], y, s=60, color="dodgerblue", label="Real")
     ax.scatter(x_2, x_1, y_p_2, s=60, color="red", label="Predicted", marker="+")
@@ -73,70 +75,161 @@ def plot_3d(x: list, y: list, y_p: list) -> None:
     plt.show()
 
 
-def run():
-    """ core method to perform the analysis """
-    df = pd.read_csv("data/time_analysis_servo_raspberry_pico.csv")
+def regression(x: np.array, y: np.array, params_model: list) -> Optional[tuple]:
+    """ minimize the mean squared error between the real values and the predicted values"""
+    try:
+        res = minimize(mse, np.array(params_model), args=(x, y), tol=1e-3)
+        y_p = model(res.x, x)
+        mae = mean_absolute_error(y, y_p)
+        return res, y_p, mae
+    except Exception:
+        return None, None, None
 
-    max_speed_servo_specs = 600
 
-    df = df[df["rotation_speed(°/s)"] <= max_speed_servo_specs]
-    df["waiting_time(ms)"] = df["waiting_time(s)"] * 1000
-    params = [290, 2]
+def clean_up_parameters(parameters: dict, max_speed_all: float, min_speed_all: float) -> dict:
+    """
+    clean up the parameters.
+    We keep the steps for which there is the minimum speed value and the maximum speed value
+    and then select the functions that fill the speed gap between them.
+    """
+    clean_parameters = {}
+
+    lst_steps = [int(i) for i in parameters.keys()]
+    lst_steps = sorted(lst_steps, reverse=True)
+
+    def add(val: int):
+        clean_parameters[val] = parameters[val]
+
+    index_min = 360
+    index_max = 0
+
+    for key, value in parameters.items():
+        if parameters[key]["max_speed"] == max_speed_all:
+            index_max = key
+
+        if parameters[key]["min_speed"] == min_speed_all:
+            index_min = key
+
+    add(index_min)
+    add(index_max)
+
+    max_speed_first = parameters[lst_steps[0]]["max_speed"]
+    min_speed_last = parameters[lst_steps[-1]]["min_speed"]
+
+    if min_speed_last < max_speed_first:
+        return clean_parameters
+
+    for step in lst_steps:
+        if step in [index_min, index_max]:
+            continue
+        max_speed = parameters[step]["max_speed"]
+        min_speed = parameters[step]["min_speed"]
+
+        if max_speed > min_speed_last and min_speed < max_speed_first:
+            add(step)
+            break
+
+    return clean_parameters
+
+
+def build_params(df: pd.DataFrame, init_params_model: list, min_mae: float,
+                 max_speed_servo_specs: int, plot_graph: bool = True) -> tuple:
+    """ do the regression and save the parameters in a config"""
+
     values = []
     parameters = {}
     min_speed_all = max_speed_servo_specs
     max_speed_all = 0
 
-    for i in range(1, df["steps"].max() + 1):
+    for i in df["steps"].unique():
+        print(f"step: {i}")
         val = df[df["steps"] == i]
         x = val["waiting_time(ms)"].to_numpy()
         y = val["rotation_speed(°/s)"].to_numpy()
-        min_speed = y.min()
-        max_speed = y.max()
 
-        min_speed_all = min(min_speed_all, min_speed)
-        max_speed_all = max(max_speed_all, max_speed)
+        res, y_p, mae = regression(x=x, y=y, params_model=init_params_model)
+        mae = min_mae + 1 if mae is None else mae
 
-        res = minimize(mse, np.array(params), args=(x, y), tol=1e-3)
+        while mae > min_mae:
+            x = x[:-1]
+            y = y[:-1]
 
-        y_p = model(res.x, x)
+            res, y_p, mae = regression(x=x, y=y, params_model=init_params_model)
+            is_none = mae
+            mae = min_mae + 1 if mae is None else mae
 
-        parameters[i] = {
-            "min_speed": min_speed,
-            "max_speed": max_speed,
-            "params": list(res.x),
-            "mae": f"{round(mean_absolute_error(y, y_p), 4)} degree/s"
-        }
+            if is_none is None or mae > min_mae or len(x) <= 3:
+                continue
 
-        values.extend([[i, x[ind - 1], y[ind - 1], y_p[ind - 1]] for ind in range(1, len(x) + 1)])
-        # print(list(res.x))
-        # plot(x, y, y_p)
+            min_speed = y.min()
+            max_speed = y.max()
+            min_speed_all = round(min(min_speed_all, min_speed), 2)
+            max_speed_all = round(max(max_speed_all, max_speed), 2)
+
+            parameters[int(i)] = {
+                "min_speed": round(min_speed, 2),
+                "max_speed": round(max_speed, 2),
+                "params": list(res.x),
+                "mae": f"{round(mean_absolute_error(y, y_p), 4)} degree/s"
+            }
+
+            values.extend([[i, x[ind - 1], y[ind - 1], y_p[ind - 1]] for ind in range(1, len(x) + 1)])
+            # plot(x, y, y_p)
+
+    if plot_graph:
+        values = np.array(values)
+        plot_3d([values[:, 0], values[:, 1]], values[:, 2], values[:, 3])
+
+    return parameters, max_speed_all, min_speed_all
+
+
+def save_params(name_servo: str, clean_parameters: dict, path_config_load: str, path_config_save: str,
+                min_speed_all: float, max_speed_all: float):
+    """ override the parameters """
+    config_analysis = load_json(path_config_load)
+    config_final = load_json(path_config_save)
+
+    config_analysis[name_servo]["speed_config"] = clean_parameters
+    del config_analysis[name_servo]["min_sleep_us"]
+    del config_analysis[name_servo]["max_sleep_us"]
+
+    config_analysis[name_servo]["min_speed_d_s"] = min_speed_all
+    config_analysis[name_servo]["max_speed_d_s"] = max_speed_all
+
+    config_final[name_servo] = config_analysis[name_servo]
+
+    save_json(path=path_config_save, json_to_save=config_final)
+
+
+def run():
+    """ core method to perform the analysis """
+
+    name_servo = "servo_sg9"
+    init_params_model = [24.36093280680071, 3.6269641385313385]
+    max_speed_servo_specs = 600
+    min_mae = 0.9
+
+    path_config_load = "./upload_to_rpp_for_data_acquisition/params/servo_params.json"
+    path_config_save = "../upload_to_raspberry_pi_pico/params/servo_params.json"
+
+    df = pd.read_csv(f"data/time_analysis_raspberry_pico_{name_servo}.csv")
+
+    df = df[df["rotation_speed(°/s)"] <= max_speed_servo_specs]
+    df["waiting_time(ms)"] = df["waiting_time(s)"] * 1000
+
+    parameters, max_speed_all, min_speed_all = \
+        build_params(
+            df=df, init_params_model=init_params_model, min_mae=min_mae,
+            max_speed_servo_specs=max_speed_servo_specs, plot_graph=True
+        )
 
     # Clean up unnecessary functions
-    clean_parameters = {}
+    clean_parameters = \
+        clean_up_parameters(parameters=parameters, max_speed_all=max_speed_all, min_speed_all=min_speed_all)
 
-    max_step_all = max([int(i) for i in parameters.keys()])
-    for step in range(1, max_step_all, 1):
-        max_speed = parameters[step]["max_speed"]
-        min_speed = parameters[step]["min_speed"]
-        clean_parameters[step] = parameters[step]
-        if min_speed <= max_speed_all <= max_speed:
-            break
-
-    name_servo = "servo_1"
-    path_config = "../upload_to_raspberry_pi_pico/params/servo_params.json"
-    config = load_json(path_config)
-    config[name_servo]["speed_config"] = clean_parameters
-
-    config[name_servo]["min_speed_d_s"] = min_speed_all
-    config[name_servo]["max_speed_d_s"] = max_speed_all
-
-    print(config)
-
-    save_json(path=path_config, json_to_save=config)
-
-    values = np.array(values)
-    plot_3d([values[:, 0], values[:, 1]], values[:, 2], values[:, 3])
+    save_params(
+        name_servo=name_servo, clean_parameters=clean_parameters, path_config_load=path_config_load,
+        path_config_save=path_config_save, min_speed_all=min_speed_all, max_speed_all=max_speed_all)
 
 
 if __name__ == '__main__':